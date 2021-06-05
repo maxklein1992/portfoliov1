2531dfa44b8f9ee4d182f2857f827078ba7efd16
Max.Klein <mcklein1992@gmail.com>
2021-06-05 12:38:14 +0200

    Added project

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,24 +1,22 @@
 export const projects = [
   {
     id: 1,
-    title: 'Enablon/Young Capital',
+    title: 'Parcel Assignment',
     image:
-      '../images/enablon.png',
-    description: ` I did a C# traineeship for Young Capital at Enablon. Currently I am an internal employer at Enablon. Enablon is an Operation Risk Management software platform company that integrates Permit to Work (PTW), Risk Assessment and Isolation Management into a single software solution`,
-    environment: ` XML, CSharp, React`,
+      '../images/parcel.png',
+    description: ` I did an Parcel assignment where every parcel should be added to the list of the correct Department. And then the department could sign the parcel. `,
+    environment: ` React, Flexbox `,
+    codelink: `https://github.com/maxklein1992/parcelapp`
   },
   {
     id: 2,
-    title: 'Mafia Go',
+    title: 'Portfolio',
     image:
-      '../images/maffiago.png',
-    description: ` Mafia Go is a smartphone app 
-    that combines gaming with the real world. 
-    The game uses location tracking and mapping
-    technology to create an 'augmented reality' where players have to capture and 
-    defence mafia buildings in real locations. Unity 3D engine is used to create the game. 
-    Google Firebase Real-time Database is a cloud-hosted NoSQL database that has been used to store and sync between our users in real-time.`,
-    environment: ` CSharp, Firebase, Unity, REST`,
+      '../images/portfolio.png',
+    description: ` This website I created in React to show my development work that I have done so far. You can find my resume and there is a possibility to contact me.`,
+    environment: ` React, MaterialUI, Flexbox`,
+    demolink: `https://portfoliov1-seven.vercel.app/`,
+    codelink: `https://github.com/maxklein1992/portfoliov1`
   },
   {
     id: 3,
@@ -31,13 +29,24 @@ export const projects = [
   },
   {
     id: 4,
-    title: 'Portfolio',
+    title: 'Enablon/Young Capital',
     image:
-      '../images/portfolio.png',
-    description: ` This website I created in React to show my development work that I have done so far. You can find my resume and there is a possibility to contact me.`,
-    environment: ` React, MaterialUI`,
-    demolink: `https://portfoliov1-seven.vercel.app/`,
-    codelink: `https://github.com/maxklein1992/portfoliov1`
+      '../images/enablon.png',
+    description: ` I did a C# traineeship for Young Capital at Enablon. Currently I am an internal employer at Enablon. Enablon is an Operation Risk Management software platform company that integrates Permit to Work (PTW), Risk Assessment and Isolation Management into a single software solution`,
+    environment: ` XML, CSharp, React`,
+  },
+  {
+    id: 5,
+    title: 'Mafia Go',
+    image:
+      '../images/maffiago.png',
+    description: ` Mafia Go is a smartphone app 
+    that combines gaming with the real world. 
+    The game uses location tracking and mapping
+    technology to create an 'augmented reality' where players have to capture and 
+    defence mafia buildings in real locations. Unity 3D engine is used to create the game. 
+    Google Firebase Real-time Database is a cloud-hosted NoSQL database that has been used to store and sync between our users in real-time.`,
+    environment: ` CSharp, Firebase, Unity, REST`,
   },
 ];
 
